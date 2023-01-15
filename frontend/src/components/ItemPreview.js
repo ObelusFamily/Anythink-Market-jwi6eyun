f47573f2ba38280c993548ed2434aed88efe1777
Matthew Davis <102203363+TeddyGavi@users.noreply.github.com>
2023-01-15 23:09:52 +0000

    remove console.log for seller

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -19,7 +19,6 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
-  console.log(item.seller)
 
 
   const handleClick = (ev) => {
@@ -48,7 +47,7 @@ const ItemPreview = (props) => {
           <h3 className="card-title">{item.title}</h3>
           <p className="card-text crop-text-3">{item.description}</p>
         </Link>
-        <div className="d-flex flex-column align-items-center pt-2 item-footer">
+        <div className="d-flex flex-row align-items-center pt-2 item-footer">
           <Link to={`/@${item.seller.username}`} className="flex-grow-1">
             <img
               src={item.seller.image}
